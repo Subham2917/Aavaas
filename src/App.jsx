088f443306f0e8ad1a2a019febbbfdf0531ef48a
subhamsingh <msubham017@gmail.com>
2024-07-16 10:53:15 +0530

    Add Header component and update App

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,19 @@ import Profile from './pages/Profile'
 import About from './pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Profile" element={<Profile />} />
         <Route path="/Sign-in" element={<SignIn />} />
-        <Route path="/Sign-up" element={<SignUp />} />
-
-
-        
+        <Route path="/Sign-up" element={<SignUp />} />        
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import {FaSearch} from "react-icons/fa"
+import { Link } from "react-router-dom"
+
+export default function Header() {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+              <Link to='/'>
+                  <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                      <span className='text-red-700'>Aavaash</span>
+                      <span className='text-slate-400'>Estate</span>
+                  </h1>
+                </Link>
+            <form action="" className='bg-slate-100 p-2 rounded-xl flex items-center'>
+                <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
+                <FaSearch className="text-slate-600"></FaSearch>
+            </form>
+            <ul className="flex gap-3">
+                <Link to='/'><li className="hidden sm:inline font-bold text-slate-700 hover:rounded-lg hover:bg-slate-400 hover:underline p-1">Home</li></Link>
+                <Link to='/about'><li className="hidden sm:inline font-bold text-slate-700 hover:bg-slate-400 hover:rounded-lg hover:underline p-1">About</li></Link>
+                <Link to='/sign-in'><li className="sm:inline font-bold text-slate-700 hover:bg-slate-400 hover:rounded-lg hover:underline p-1">SignIn</li></Link>
+            </ul>
+        </div>        
+    </header>
+  )
+}
