5b60c04da44ee654a0d0822a91f4379c3986ea6d
subhamsingh <msubham017@gmail.com>
2024-07-17 16:34:15 +0530

    cfeate signup api route

--- api/index.js
+++ api/index.js
@@ -2,10 +2,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('connected to mongodb!')
 }).catch((err) =>{
@@ -18,3 +22,4 @@ app.listen(3000, () =>{
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
