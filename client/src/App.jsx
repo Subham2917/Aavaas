6bf80bea59b8cdadf332c3baae0a154a219adc14
subhamsingh <msubham017@gmail.com>
2024-08-05 14:51:15 +0530

    complete image slider functionality to listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
+import Listing from './pages/Listing'
 
 
 export default function App() {
@@ -17,6 +18,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/Profile" element={<Profile />} />
           <Route path="/Create-listing" element={<CreateListing />} />
